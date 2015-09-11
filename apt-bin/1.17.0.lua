7839352c22fc3d8212c0b53ac636da00b0cd331b
Adam Huffman <bloch@verdurin.com>
2015-09-11 12:32:50 +0100

    Remove aclocal and change path for Van Loo

--- apt-bin/1.17.0.lua
+++ apt-bin/1.17.0.lua
@@ -6,9 +6,8 @@ whatis([[Description:  Affymetrix Power Tools (APT) are a set of cross-platform 
 implement algorithms for analyzing and working with Affymetrix GeneChip® arrays. ]])
 whatis([[Homepage: http://www.affymetrix.com/estore/partners_programs/programs/developer/tools/powertools.affx]])
 
-local root = "/opt/soft/apps/apt-bin/1.17.0"
+local root = "/srv/data/sw/apps/apt-bin/1.17.0"
 
-prepend_path("ACLOCAL_PATH", pathJoin(root, "share/aclocal"))
 prepend_path("PATH", pathJoin(root, "bin"))
 setenv("EBROOTAPTBIN", root)
 setenv("EBVERSIONAPTBIN", "1.17.0")
